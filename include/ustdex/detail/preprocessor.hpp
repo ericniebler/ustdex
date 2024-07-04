2d0b7d3d6cdfb7b331132457bda1b660dbea9d2a
Eric Niebler <eniebler@nvidia.com>
2024-07-04 10:29:56 -0700

    work around buggy msvc preprocessor

--- include/ustdex/detail/preprocessor.hpp
+++ include/ustdex/detail/preprocessor.hpp
@@ -10,15 +10,17 @@
 #pragma once
 
 // clang-format off
+#define USTDEX_PP_EAT(...)
+#define USTDEX_PP_EMPTY()
 #define USTDEX_PP_STRINGIZE(...) #__VA_ARGS__
 #define USTDEX_PP_CAT_I(A, ...)  A##__VA_ARGS__
 #define USTDEX_PP_CAT(A, ...)    USTDEX_PP_CAT_I(A, __VA_ARGS__)
 #define USTDEX_PP_EVAL(M, ...)   M(__VA_ARGS__)
 #define USTDEX_PP_EXPAND(...)    __VA_ARGS__
-#define USTDEX_PP_EAT(...)
+#define USTDEX_PP_DEFER(M)       M USTDEX_PP_EMPTY()
 
-#define USTDEX_PP_CHECK(...)          USTDEX_PP_EXPAND(USTDEX_PP_CHECK_(__VA_ARGS__, 0, ))
+#define USTDEX_PP_CHECK(...)          USTDEX_PP_EXPAND(USTDEX_PP_DEFER(USTDEX_PP_CHECK_)(__VA_ARGS__, 0, ))
 #define USTDEX_PP_CHECK_(XP, NP, ...) NP
-#define USTDEX_PP_PROBE(...)          USTDEX_PP_PROBE_(__VA_ARGS__, 1)
+#define USTDEX_PP_PROBE(XP, NP)       USTDEX_PP_PROBE_(XP, NP, 1)
 #define USTDEX_PP_PROBE_(XP, NP, ...) XP, NP,
 // clang-format on
