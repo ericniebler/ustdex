c230a41d6a7b13196502581c7454e415128be33c
Eric Niebler <eniebler@nvidia.com>
2025-03-26 09:42:34 -0700

    clean up `_rcvr_with_env_t` and fix portability issues

--- include/ustdex/detail/rcvr_with_env.hpp
+++ include/ustdex/detail/rcvr_with_env.hpp
@@ -28,107 +28,54 @@ namespace ustdex
 template <class Rcvr, class Env>
 struct _rcvr_with_env_t : Rcvr
 {
-  using _env_t = _rcvr_with_env_t const&;
+  struct _env_t
+  {
+    template <class Query>
+    USTDEX_TRIVIAL_API static constexpr decltype(auto) _get_1st(const _env_t& self) noexcept
+    {
+      if constexpr (_queryable_with<Env, Query>)
+      {
+        return (self._rcvr_->_env_);
+      }
+      else if constexpr (_queryable_with<env_of_t<Rcvr>, Query>)
+      {
+        return ustdex::get_env(static_cast<const Rcvr&>(*self._rcvr_));
+      }
+    }
 
-  USTDEX_TRIVIAL_API auto _rcvr() noexcept -> Rcvr&
+    template <class Query>
+    using _1st_env_t = decltype(_env_t::_get_1st<Query>(declval<const _env_t&>()));
+
+    template <class Query>
+    USTDEX_TRIVIAL_API constexpr auto query(Query) const noexcept(_nothrow_queryable_with<_1st_env_t<Query>, Query>) //
+      -> _query_result_t<_1st_env_t<Query>, Query>
+    {
+      return _env_t::_get_1st<Query>(*this);
+    }
+
+    _rcvr_with_env_t const* _rcvr_;
+  };
+
+  USTDEX_TRIVIAL_API auto base() && noexcept -> Rcvr&&
+  {
+    return static_cast<Rcvr&&>(*this);
+  }
+
+  USTDEX_TRIVIAL_API auto base() & noexcept -> Rcvr&
   {
     return *this;
   }
 
-  USTDEX_TRIVIAL_API auto _rcvr() const noexcept -> const Rcvr&
+  USTDEX_TRIVIAL_API auto base() const& noexcept -> Rcvr const&
   {
     return *this;
   }
 
   USTDEX_TRIVIAL_API auto get_env() const noexcept -> _env_t
   {
-    return _env_t{*this};
+    return _env_t{this};
   }
 
-  template <class Query>
-  USTDEX_TRIVIAL_API static constexpr decltype(auto) _get_1st(const _rcvr_with_env_t& _self) noexcept
-  {
-    if constexpr (_queryable_with<Env, Query>)
-    {
-      return (_self._env_);
-    }
-    else if constexpr (_queryable_with<env_of_t<Rcvr>, Query>)
-    {
-      return ustdex::get_env(static_cast<const Rcvr&>(_self));
-    }
-  }
-
-  template <class Query>
-  using _1st_env_t = decltype(_rcvr_with_env_t::_get_1st<Query>(declval<const _rcvr_with_env_t&>()));
-
-  template <class Query>
-  USTDEX_TRIVIAL_API constexpr auto query(Query _query) const
-    noexcept(_nothrow_queryable_with<_1st_env_t<Query>, Query>) //
-    -> _query_result_t<_1st_env_t<Query>, Query>
-  {
-    return _rcvr_with_env_t::_get_1st<Query>(*this).query(_query);
-  }
-
-  Env _env_;
-};
-
-template <class Rcvr, class Env>
-struct _rcvr_with_env_t<Rcvr*, Env>
-{
-  using _env_t = _rcvr_with_env_t const&;
-
-  USTDEX_TRIVIAL_API auto _rcvr() const noexcept -> Rcvr*
-  {
-    return _rcvr_;
-  }
-
-  template <class... As>
-  USTDEX_TRIVIAL_API void set_value(As&&... _as) && noexcept
-  {
-    ustdex::set_value(_rcvr_, static_cast<As&&>(_as)...);
-  }
-
-  template <class Error>
-  USTDEX_TRIVIAL_API void set_error(Error&& _error) && noexcept
-  {
-    ustdex::set_error(_rcvr_, static_cast<Error&&>(_error));
-  }
-
-  USTDEX_TRIVIAL_API void set_stopped() && noexcept
-  {
-    ustdex::set_stopped(_rcvr_);
-  }
-
-  USTDEX_TRIVIAL_API auto get_env() const noexcept -> _env_t
-  {
-    return _env_t{*this};
-  }
-
-  template <class Query>
-  USTDEX_TRIVIAL_API constexpr decltype(auto) _get_1st(Query) const noexcept
-  {
-    if constexpr (_queryable_with<Env, Query>)
-    {
-      return (_env_);
-    }
-    else if constexpr (_queryable_with<env_of_t<Rcvr>, Query>)
-    {
-      return ustdex::get_env(_rcvr_);
-    }
-  }
-
-  template <class Query, class Self = _rcvr_with_env_t>
-  using _1st_env_t = decltype(declval<const Self&>()._get_1st(Query{}));
-
-  template <class Query>
-  USTDEX_TRIVIAL_API constexpr auto query(Query _query) const
-    noexcept(_nothrow_queryable_with<_1st_env_t<Query>, Query>) //
-    -> _query_result_t<_1st_env_t<Query>, Query>
-  {
-    return _get_1st(_query).query(_query);
-  }
-
-  Rcvr* _rcvr_;
   Env _env_;
 };
 
